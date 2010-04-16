e331e95cb2e16198d74d400b3100c2229e46b650
Thomas Quémard <thomas.quemard@gmail.com>
2010-04-16 22:37:20 +0200

    +php to class check

--- src/smoid/languages/klass.py
+++ src/smoid/languages/klass.py
@@ -10,16 +10,28 @@ class KlassCheck (Check):
 
         self.name = "Class"
         self.example = "public class GreatClassRight : DadClass"
-        self.add_language("java")
         self.add_language("csharp")
+        self.add_language("java")
+        self.add_language("php")
+        self.add_language("scala")
 
-        self.java_modifiers = ["abstract", "public", "private"]
-        self.csharp_modifiers = ["abstract", "public", "private", "protected", "internal"]
+        # http://www.javacamp.org/javaI/Modifier.html
+        self.java_modifiers = ["abstract", "final", "public", "private", "static", "strictfp"]
 
-        class_modifiers = ["abstract", "public", "private", "protected", "internal"]
+        # http://msdn.microsoft.com/en-us/library/0b0thckt%28VS.71%29.aspx
+        self.csharp_modifiers = ["abstract", "new", "public", "private", "protected", "internal", "sealed"]
+
+        self.php_modifiers = ["abstract"]
+
+        self.scala_modifiers = ["private", "protected"]
+
+        prefix_modifiers = []
+        prefix_modifiers.extend(self.csharp_modifiers)
+        prefix_modifiers.extend(self.java_modifiers)
+        prefix_modifiers.extend(self.php_modifiers)
 
         res_modifiers = ""
-        for modifier in class_modifiers:
+        for modifier in prefix_modifiers:
             if res_modifiers != "":
                 res_modifiers += "|"
             res_modifiers += "(?:" + modifier + "\s+)"
@@ -37,11 +49,20 @@ class KlassCheck (Check):
         for match in matches:
             self.incr_language_probability("java", 20)
             self.incr_language_probability("csharp", 20)
+            self.incr_language_probability("php", 20)
 
-            modifiers = match.split(" ")
-            for modifier in modifiers:
-                if modifier.strip() in self.java_modifiers:
-                    self.incr_language_probability("java", 10)
+            if match != "":
+                modifiers = match.split(" ")
+                for modifier in modifiers:
+                    modifier = modifier.strip()
+                    if modifier in self.java_modifiers:
+                        self.incr_language_probability("java", 10)
 
-                if modifier.strip() in self.csharp_modifiers:
-                    self.incr_language_probability("csharp", 10)
+                    if modifier in self.csharp_modifiers:
+                        self.incr_language_probability("csharp", 10)
+
+                    if modifier in self.php_modifiers:
+                        self.incr_language_probability("php", 10)
+
+            else:
+                self.incr_language_probability("scala", 20)
