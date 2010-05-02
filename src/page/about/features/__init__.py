2caefba1d0754dede1d4166be8d4d9f20c75a910
Thomas Quémard <thomas.quemard@gmail.com>
2010-05-02 10:43:22 +0200

    beauty contest

--- src/page/about/features/__init__.py
+++ src/page/about/features/__init__.py
@@ -1,3 +1,5 @@
+# Copyright 2008 Thomas Quemard
+#
 # Paste-It is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published
 # by the Free Software Foundation; either version 3.0, or (at your option)
@@ -8,11 +10,11 @@
 # or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
 # License for more details.
 
+
 import cgi
 
-import smoid.languages
-import app.model
 import app.web
+
 
 class Features (app.web.RequestHandler):
 
@@ -22,4 +24,4 @@ class Features (app.web.RequestHandler):
         self.path.add("About", app.url("about"))
         self.path.add("Features", app.url("about/features"))
 
-        self.write_out("page/about/features/200.html")
+        self.write_out("./200.html")
